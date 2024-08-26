cb316e8830d6ee55dd878bdabe06d62634d7db68
Elena Ghencea <elena.ghencea@ama.gv.at>
2024-08-26 14:55:35 +0200

    WIP

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,20 +1,26 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatSidenav} from "@angular/material/sidenav";
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { MatSidenav } from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit{
+export class NavigationComponent implements AfterViewInit {
 
   @ViewChild(MatSidenav) sidenav: MatSidenav;
 
   constructor() {}
 
-  ngOnInit() {}
-// recherche machen
+  ngAfterViewInit() {
+    // sidenav ist nun initialisiert
+  }
+
   toggleSidenav() {
-    this.sidenav.toggle();
+    if (this.sidenav) {
+      this.sidenav.toggle();
+    } else {
+      console.warn("Sidenav ist noch nicht initialisiert");
+    }
   }
 }
